2d04d28aef4f3cfa40d13913b42ca45487b83140
difasyafitri4 <158707251+difasyafitri4@users.noreply.github.com>
2024-02-17 16:45:09 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,5 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('feed',FeedController::class);
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
